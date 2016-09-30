d21c81e210447839dc5093d77c0c399f14496211
zhangmingwei <zjjzmw1@163.com>
2016-09-30 17:48:56 +0800

    添加segment OK

--- ZMWJokeOC/Collection/CollectionViewController.m
+++ ZMWJokeOC/Collection/CollectionViewController.m
@@ -8,8 +8,11 @@
 
 #import "CollectionViewController.h"
 #import <HMSegmentedControl.h>
+#import <Masonry.h>
 
 @interface CollectionViewController ()
+
+@property (nonatomic, strong) HMSegmentedControl *segmentedControl;
 
 @end
 
@@ -18,15 +21,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.title = @"收藏";
-
+    [self initSegment];
 }
 
 #pragma mark - 初始化segment
 - (void)initSegment {
-    HMSegmentedControl *segmentedControl = [[HMSegmentedControl alloc] initWithSectionTitles:@[@"One", @"Two", @"Three"]];
-    segmentedControl.frame = CGRectMake(10, 10, 300, 60);
-    [segmentedControl addTarget:self action:@selector(segmentedControlChangedValue:) forControlEvents:UIControlEventValueChanged];
-    [self.view addSubview:segmentedControl];
+    __weak typeof(self) wSelf = self;
+    self.segmentedControl = [[HMSegmentedControl alloc] initWithSectionTitles:@[@"文字", @"图片"]];
+    [self.view addSubview:self.segmentedControl];
+    [self.segmentedControl mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(wSelf.view).offset(0);
+        make.right.equalTo(wSelf.view).offset(0);
+        make.top.equalTo(wSelf.view).offset(NAVIGATIONBAR_HEIGHT);
+        make.height.mas_equalTo(44);
+    }];
+    [self.segmentedControl addTarget:self action:@selector(segmentedControlChangedValue:) forControlEvents:UIControlEventValueChanged];
+    self.segmentedControl.selectionIndicatorColor = [UIColor blueColor];
+    self.segmentedControl.backgroundColor = self.view.backgroundColor;
+    self.segmentedControl.selectionIndicatorLocation = HMSegmentedControlSelectionIndicatorLocationDown;
+    
+}
+
+- (void)segmentedControlChangedValue:(HMSegmentedControl *)segmentControl {
+    
 }
 
 @end
